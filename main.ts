21c45225ab1c9ca71f7d0ca83560e56e5ce3f639
Jaydin Madore <92885447+Muddysprings@users.noreply.github.com>
2021-11-09 14:24:20 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,48 @@
 let Degrees = 0
 basic.forever(function () {
+    if (Degrees < 45) {
+        pins.digitalWritePin(DigitalPin.P2, 1)
+        led.enable(true)
+    }
+})
+basic.forever(function () {
     Degrees = input.compassHeading()
     if (Degrees < 45) {
-        basic.showString("N")
+        basic.showString("N 45-134")
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . . . #
+            . . # . .
+            . . # . .
+            `)
     } else if (Degrees < 135) {
-        basic.showString("E")
+        basic.showString("E 135-224")
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . . . #
+            . . # . .
+            . . # . .
+            `)
     } else if (Degrees < 225) {
-        basic.showString("S")
+        basic.showString("S 225-314")
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . . . #
+            . . # . .
+            . . # . .
+            `)
     } else if (Degrees < 315) {
-        basic.showString("W")
+        basic.showString("W 315-44")
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . . . #
+            . . # . .
+            . . # . .
+            `)
     } else {
         basic.showString("N")
     }
